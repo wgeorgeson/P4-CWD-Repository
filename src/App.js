a6cd0ff239950bbdc1032110bc184af237e40fed
William Georgeson <wgeorgeson@madisoncollege.edu>
2022-12-14 18:39:20 -0600

    Modified src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cubs rule!!
         </p>
         <a
           className="App-link"
